f5c9e2068e6eef30829b37020ecda42d10d08de3
Andrew Mathew <80082032+andrewgithb@users.noreply.github.com>
2023-09-22 14:39:39 -0400

    updated URL

--- server.py
+++ server.py
@@ -16,7 +16,7 @@ app = Flask(__name__,
             static_url_path='',
             static_folder='')
 
-YOUR_DOMAIN = 'http://localhost:4242'
+YOUR_DOMAIN = 'https://andrewgithb.github.io/shopping-demo/'
 
 @app.route('/hoodie-buy', methods=['POST'])
 #@app.route('/create-checkout-session', methods=['POST'])
